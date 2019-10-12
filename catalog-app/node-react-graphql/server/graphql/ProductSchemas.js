67076b83d96a31c405031d1c9ca400432b02d0c3
MattisTrygstad <mattislt@sf-nett.no>
2019-10-12 13:02:47 +0200

    GraphQL successfull queries

--- catalog-app/node-react-graphql/server/graphql/ProductSchemas.js
+++ catalog-app/node-react-graphql/server/graphql/ProductSchemas.js
@@ -13,91 +13,244 @@ var productType = new GraphQLObjectType({
     name: 'product',
     fields: function () {
       return {
-        _varenummer: {
+        Varenummer: {
           type: GraphQLString
         },
-        varenavn: {
+        Varenavn: {
           type: GraphQLString
         },
-        volum: {
+        Volum: {
           type: GraphQLString
         },
-        pris: {
+        Pris: {
           type: GraphQLString
         },
-        literpris: {
+        Literpris: {
           type: GraphQLString
         },
-        varetype: {
+        Varetype: {
           type: GraphQLString
         },
-        produktutvalg: {
+        Produktutvalg: {
             type: GraphQLString
         },
-        fylde: {
+        Fylde: {
             type: GraphQLString
         },
-        friskhet: {
+        Friskhet: {
             type: GraphQLString
         },
-        garvestoffer: {
+        Garvestoffer: {
             type: GraphQLString
         },
-        bitterhet: {
+        Bitterhet: {
             type: GraphQLString
         },
-        sodme: {
+        Sodme: {
             type: GraphQLString
         },
-        farge: {
+        Farge: {
             type: GraphQLString
         },
-        lukt: {
+        Lukt: {
             type: GraphQLString
         },
-        smak: {
+        Smak: {
             type: GraphQLString
         },
-        land: {
+        Land: {
             type: GraphQLString
         },
-        argang: {
+        Argang: {
             type: GraphQLString
         }
       }
     }
   });
 
-  var queryType = new GraphQLObjectType({
-    name: 'Query',
-    fields: function () {
-      return {
-        products: {
-          type: new GraphQLList(productType),
-          resolve: function () {
-            const products = ProductModel.find().exec()
-            if (!products) {
-              throw new Error('Error')
-            }
-            return products
+var queryType = new GraphQLObjectType({
+name: 'Query',
+fields: function () {
+    return {
+    products: {
+        type: new GraphQLList(productType),
+        resolve: function () {
+        const products = ProductModel.find().exec()
+        console.log("success");
+        
+        if (!products) {
+            throw new Error('Error')
+        }
+        return products
+        }
+    },
+    product: {
+        type: productType,
+        args: {
+        Varenummer: {
+            name: 'Varenummer',
+            type: GraphQLString
+        }
+        },
+        resolve: function (root, params) {
+        const productDetails = ProductModel.find(params.Varenummer).exec()
+        if (!productDetails) {
+            throw new Error('Error')
+        }
+        return productDetails
+        }
+    }
+    }
+}
+});
+
+var mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: function () {
+    return {
+      addProduct: {
+        type: productType,
+        args: {
+          Varenummer: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Varenavn: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Volum: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Pris: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Literpris: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Varetype: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Produktutvalg: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Fylde: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Friskhet: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Garvestoffer: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Bitterhet: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Sodme: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Farge: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Lukt: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Smak: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Land: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Argang: {
+            type: new GraphQLNonNull(GraphQLString)
           }
         },
-        product: {
-          type: productType,
-          args: {
-            id: {
-              name: '_varenummer',
-              type: GraphQLString
-            }
-          },
-          resolve: function (root, params) {
-            const productDetails = ProductModel.findById(params.varenummer).exec()
-            if (!productDetails) {
-              throw new Error('Error')
-            }
-            return productDetails
+        resolve: function (root, params) {
+          const productModel = new ProductModel(params);
+          const newProduct = productModel.save();
+          if (!newProduct) {
+            throw new Error('Error');
           }
+          return newProduct
+        }
+      },
+      updateBook: {
+        type: productType,
+        args: {
+          Varenummer: {
+            name: 'Varenummer', // id?
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Varenavn: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Volum: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Pris: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Literpris: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Varetype: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Produktutvalg: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Fylde: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Friskhet: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Garvestoffer: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Bitterhet: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Sodme: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Farge: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Lukt: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Smak: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Land: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          Argang: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve(root, params) {
+          return ProductModel.findByIdAndUpdate(params.varenummer, { Varenavn: params.Varenavn, Volum: params.Volum, Pris: params.Pris, Literpris: params.Literpris, Varetype: params.Varetype, Produktutvalg: params.Produktutvalg, Fylde: params.Fylde, Friskhet: params.Friskhet, Garvestoffer: params.Garvestoffer, Bitterhet: params.Bitterhet, Sodme: params.Sodme, Farge: params.Farge, Smak: params.Smak, Land: params.Land, Argang: params.Argang }, function (err) {
+            if (err) return next(err);
+          });
+        }
+      },
+      removeBook: {
+        type: productType,
+        args: {
+          Varenummer: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve(root, params) {
+          const removeProduct = ProductModel.findByIdAndRemove(params.varenummer).exec();
+          if (!removeProduct) {
+            throw new Error('Error')
+          }
+          return removeProduct;
         }
       }
     }
-  });
+  }
+});
+
+module.exports = new GraphQLSchema({query: queryType, mutation: mutation});
--- catalog-app/node-react-graphql/server/models/Product.js
+++ catalog-app/node-react-graphql/server/models/Product.js
@@ -1,23 +1,23 @@
 var mongoose = require('mongoose');
 
 var ProductSchema = new mongoose.Schema({
-    varenummer: String,
-    varenavn: String,
-    volum: String,
-    pris: String,
-    literpris: String,
-    varetype: String,
-    produktutvalg: String,
-    fylde: String,
-    friskhet: String,
-    garvestoffer: String,
-    bitterhet: String,
-    sodme: String,
-    farge: String,
-    lukt: String,
-    smak: String,
-    land: String,
-    argang: String,
+    Varenummer: String,
+    Varenavn: String,
+    Volum: String,
+    Pris: String,
+    Literpris: String,
+    Varetype: String,
+    Produktutvalg: String,
+    Fylde: String,
+    Friskhet: String,
+    Garvestoffer: String,
+    Bitterhet: String,
+    Sodme: String,
+    Farge: String,
+    Lukt: String,
+    Smak: String,
+    Land: String,
+    Argang: String,
   });
   
-  module.exports = mongoose.model('Book', ProductSchema);
+  module.exports = mongoose.model('product', ProductSchema);
