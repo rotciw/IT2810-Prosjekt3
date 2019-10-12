a02376d0fa2365227b162c0989ad4717d641ceb2
Wictor <wictor_x@hotmail.com>
2019-10-12 16:40:37 +0200

    #5 initial set up of table

---
+++ catalog-app/src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomTable from '../table/CustomTable';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomTable/>
+    </div>
+  );
+}
+
+export default App;
