7148eb6c2be0cc98dfc7819b1ffe32a962e7113b
MattisTrygstad <mattislt@sf-nett.no>
2019-10-13 15:47:40 +0200

    ApolloClient setup #14

--- catalog-app/src/components/app/App.js
+++ catalog-app/src/components/app/App.js
@@ -1,13 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
-import CustomTable from '../table/CustomTable'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomTable/>
-    </div>
-  );
+const GET_PRODUCTS = gql`
+  {
+    products {
+      Varenummer
+      Varenavn
+      Volum
+      Pris
+      Literpris
+      Varetype
+      Produktutvalg
+      Fylde
+      Friskhet
+      Garvestoffer
+      Bitterhet
+      Sodme
+      Smak
+      Land
+      Argang
+      Rastoff
+      Alkohol
+      Emballasjetype
+      Vareurl
+    }
+  }
+`;
+
+const GET_PRODUCTQUERY = gql`
+  {
+    productQuery(Argang: "2019", skipping: 0) {
+      Varenummer
+      Varenavn
+      Volum
+      Pris
+      Literpris
+      Varetype
+      Produktutvalg
+      Fylde
+      Friskhet
+      Garvestoffer
+      Bitterhet
+      Sodme
+      Smak
+      Land
+      Argang
+      Rastoff
+      Alkohol
+      Emballasjetype
+      Vareurl
+    }
+  }
+`;
+export default class CustomTable extends Component {
+  render() {
+    return (
+      <Query pollInterval={500} query={GET_PRODUCTQUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+    
+          return (
+            <div className="container">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3 className="panel-title">
+                    LIST OF PRODUCTS
+                  </h3>
+                  <h4><Link to="/create">Add Product</Link></h4>
+                </div>
+                <div className="panel-body">
+                  <table className="table table-stripe">
+                    <thead>
+                      <tr>
+                        <th>Varenummer</th>
+                        <th>Varenavn</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data.products.map((product, index) => (
+                        <tr key={index}>
+                          <td><Link to={`/show/${product.Varenummer}`}>{product.Varenavn}</Link></td>
+                          <td>{product.Varenavn}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
 }
-
-export default App;
--- catalog-app/src/index.js
+++ catalog-app/src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
+import Create from './components/Create';
+import Show from './components/Show';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({ uri: 'http://localhost:3000/graphql' });
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Router>
+            <div>
+                <Route exact path='/' component={App} />
+                <Route path='/show/:id' component={Show} />
+                
+            </div>
+        </Router>
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
+
+//<Route path='/create' component={Create} /> 
+//<Route path='/show/:id' component={Show} />
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
