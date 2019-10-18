6b65f279da26e74fa3db18e28e6ee4402fe49b0d
Kasper Maagerø Svendsen <kasperms@stud.ntnu.no>
2019-10-18 13:15:39 +0200

    Result per page set to 20 instead of 50

--- catalog-app/node-react-graphql/server/graphql/schemas.js
+++ catalog-app/node-react-graphql/server/graphql/schemas.js
@@ -166,7 +166,7 @@ fields: function () {
                 [{Varetype: { $regex: ".*"+params.Keys+".*",'$options' : 'i' }},
                 {Varenavn: { $regex: ".*"+params.Keys+".*",'$options' : 'i' }},
                 {Land: { $regex: ".*"+params.Keys+".*",'$options' : 'i' }}])
-                .sort(params.SortAfter).limit(50).skip(50*params.Skipping).exec()
+                .sort(params.SortAfter).limit(20).skip(20*params.Skipping).exec()
             if (!products) {
                 throw new Error('Error')
             }
