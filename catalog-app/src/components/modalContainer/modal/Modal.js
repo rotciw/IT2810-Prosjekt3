ec299490799c76d0a7e8b001097ede42e65fcfb4
Wictor <wictor_x@hotmail.com>
2019-10-23 12:11:03 +0200

    #37 Add responsive wordcloud as advanced viewing

--- catalog-app/src/components/modalContainer/modal/Modal.js
+++ catalog-app/src/components/modalContainer/modal/Modal.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import './Modal.css'
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo';
+import ReactWordCloud from 'react-wordcloud';
 
-function modal(props) {
+const GET_POPULAR = gql`
+{
+  popularSearches{
+    Searched,
+    Times
+  }
+}`;
+
+function Modal(props) {
+    const { loading, error, data } = useQuery(GET_POPULAR)
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    let cloudData = []
+    data.popularSearches.map(popSearch => (
+        cloudData.push({
+            text:popSearch.Searched,
+            value:popSearch.Times
+        })
+
+    ))
+
     return (
         <div>
-            <div className="modal-wrapper"
+            <div className="modalWrapper"
             style={{
                 visibility: props.show ? 'visible' : 'hidden',
             }}>
-            <div className="modal-header">
-                <h3>Avansert visning (word cloud):</h3>
-                <span className="close-modal-btn" onClick={props.close}>×</span>
+            <div className="modalHeader">
+                <span className="closeModalBtn" onClick={props.close}>×</span>
+                <h3>Mest populære søk:</h3>
             </div>
-            <div className="modal-body">
-                <p>
-                    {props.children}
-                </p>
+            <div className="modalBody">
+                <ReactWordCloud
+                    words={cloudData}
+                    options={{fontSizes: [16,60]}}
+                />
             </div>
         </div>
     </div>
     )
 }
 
-export default modal;
+export default Modal;
