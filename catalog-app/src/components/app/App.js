4ea5eaf510a2f185b466b27b72320bf5008f6305
Wictor <wictor_x@hotmail.com>
2019-10-24 17:58:27 +0200

    #49 Make own RootStore component and clean up app.js

--- catalog-app/src/components/app/App.js
+++ catalog-app/src/components/app/App.js
@@ -1,65 +1,15 @@
 import React from 'react';
 import './App.css';
-import { decorate, observable, action } from 'mobx';
 import { Provider } from 'mobx-react';
 import Table from '../table/Table';
-import FilterStore from '../../stores/FilterStore';
-import SearchBarStore from '../../stores/SearchBarStore';
-import SortStore from '../../stores/SortStore';
-import PaginationStore from '../../stores/PaginationStore';
 import FilterGroup from '../filterGroup/FilterGroup';
 import Header from '../header/Header';
 import ModalContainer from '../modalContainer/ModalContainer';
 import SearchBar from '../searchBar/SearchBar';
 import Pagination from '../pagination/Pagination';
 import SortDropdown from '../sortDropdown/SortDropdown';
+import RootStore from '../../stores/RootStore';
 
-decorate(FilterStore, {
-  countryFilter: observable,
-  packagingFilter: observable,
-  productSelectionFilter: observable,
-  yearMinFilter: observable,
-  yearMaxFilter: observable,
-  priceMinFilter: observable,
-  priceMaxFilter: observable,
-  addCountryFilter: action,
-  addPackagingFilter: action,
-  addProductSelectionFilter: action,
-  addYearMinFilter: action,
-  addYearMaxFilter: action,
-  addPriceMinFilter: action,
-  addPriceMaxFilter: action,
-})
-decorate(SortStore, {
-  sortAfter: observable,
-  activeButton: observable,
-  addSortAfter: action,
-  addActiveButton: action
-})
-decorate(SearchBarStore, {
-  searchBarValue: observable,
-  addSearchBarValue: action,
-})
-decorate(PaginationStore, {
-  paginationPage: observable,
-  prevButtonDisabled: observable,
-  nextButtonDisabled: observable,
-  tableIsEmpty: observable,
-  incrementPage: action,
-  decrementPage: action,
-  firstPage: action,
-  tableEmpty: action,
-  tableNotEmpty: action
-})
-
-class RootStore {
-  constructor(){
-    this.filterStore = new FilterStore();
-    this.sortStore = new SortStore();
-    this.searchBarStore = new SearchBarStore();
-    this.paginationStore = new PaginationStore();
-  }
-}
 const rootStore = new RootStore();
 
 function App() {
