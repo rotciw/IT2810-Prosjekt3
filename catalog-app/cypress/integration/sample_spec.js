7f6f32c9c03b1dcfe44b517170423e9419879d2e
Kasper Maagerø Svendsen <kasperms@stud.ntnu.no>
2019-10-24 18:17:26 +0200

    Removed example texts

--- catalog-app/cypress/integration/sample_spec.js
+++ catalog-app/cypress/integration/sample_spec.js
@@ -1,6 +1,7 @@
 describe('Visit studentpolet', function() {
     it('Visit studentpolet!', function() {
-        cy.visit('localhost:3001');
+        cy.visit('http://it2810-38.idi.ntnu.no/prosjekt3/');
         cy.get('#searchBar').type('Gin').should('have.value', 'Gin');
+        cy.get('#button').click();
     })
   })
