b16a70d6b981ac90b6d5cbd5e210e22ea75b2bfa
MattisTrygstad <mattislt@sf-nett.no>
2019-10-25 14:20:42 +0200

    added snapshot tests #50

---
+++ catalog-app/src/tests/SearchBar.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SearchBar from '../components/searchBar/SearchBar.js';
+import { Provider } from 'mobx-react';
+import RootStore from '../stores/RootStore';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+    link: createHttpLink({
+        uri: 'http://it2810-38.idi.ntnu.no:3000/graphql',
+        fetch: fetch,
+    }),
+    cache: cache,
+});
+const rootStore = new RootStore();
+
+it('renders correctly', () => {
+    const tree = renderer
+        .create(
+            <ApolloProvider client={client}>
+                <Provider
+                    rootStore={rootStore}
+                    filterStore={rootStore.filterStore}
+                    sortStore={rootStore.sortStore}
+                    searchBarStore={rootStore.searchBarStore}
+                    paginationStore={rootStore.paginationStore}>
+                    <SearchBar/>
+                </Provider>
+            </ApolloProvider>
+            )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
